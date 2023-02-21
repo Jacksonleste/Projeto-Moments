91888d80410c24181158bb72817357947d2240b1
Jackson Leste <jacksongoncalves.leste@gmail.com>
2023-02-20 21:37:53 -0300

    montando formulario de inserção do momento

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewMomentComponent implements OnInit {
 
+  btnText:string = "compartilhar!";
+
   constructor() { }
 
   ngOnInit(): void {
